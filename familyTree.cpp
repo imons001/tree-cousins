6c8ed6b7d470f4c927a5a35db5ea2f274faeb37a
imons001 <94793634+imons001@users.noreply.github.com>
2023-07-03 19:49:56 -0400

    commited

--- familyTree.cpp
+++ familyTree.cpp
@@ -21,5 +21,49 @@ FamilyTree::~FamilyTree()
         delete child;
 }
 
+int FamilyTree::numberOfCousins() const {
+    if(parent == nullptr || parent->parent == nullptr){
+        return 0;
+    }
+    
+    FamilyTree *grandparent = parent->parent;
+    int cousinsCount = 0;
+
+    for (FamilyTree *uncleOrAunt : grandparent->children){
+        if (uncleOrAunt == parent)
+            continue;{
+        cousinsCount += uncleOrAunt->children.size();
+        }
+    }
+
+    return cousinsCount;
+}
+
+FamilyTree *FamilyTree::find(int identifier) {
+    if (this->identifier == identifier)
+        return this;
+
+    for (FamilyTree *child : children) {
+        FamilyTree *result = child->find(identifier);
+        if (result != nullptr)
+            return result;
+    }
+
+    return nullptr;
+}
+
+const FamilyTree *FamilyTree::find(int identifier) const {
+    if (this->identifier == identifier)
+        return this;
+
+    for (FamilyTree *child : children) {
+        const FamilyTree *result = child->find(identifier);
+        if (result != nullptr)
+            return result;
+    }
+
+    return nullptr;
+}
+
 
 
